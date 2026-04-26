25abb4fb92eefa9e107176646466da22520fbace
shahrukh <southsec021karachi@gmail.com>
2026-04-26 17:13:46 +0500

    Changed the colour of add to cart buttton

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -175,7 +175,7 @@ const ProductDetailPage = () => {
           <div className="space-y-3 mt-6">
             <button
               onClick={handleAddToCart}
-              className="bg-green-600 hover:bg-blue-700 text-black font-bold py-3 px-6 rounded-lg w-full transition"
+              className="bg-green-600 hover:bg-green-700 text-black font-bold py-3 px-6 rounded-lg w-full transition"
               disabled={!selectedSize}
             >
               🛒 Add to Cart
